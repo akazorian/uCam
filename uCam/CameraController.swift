8515478447bddd2204f6524b4cb1cd5b1d7dce7e
Alex Kazorian <Alex_Kazorian@Alexs-MacBook-Pro-2.local>
2016-06-08 15:40:46 -0400

    Camera button is a visible, transparent, circle

--- uCam/CameraController.swift
+++ uCam/CameraController.swift
@@ -8,27 +8,38 @@
 
 import UIKit
 import AVFoundation
+import SnapKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class CameraController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     var captureSession : AVCaptureSession?
     var stillImageOutput : AVCaptureStillImageOutput?
     var previewLayer : AVCaptureVideoPreviewLayer?
     var image : UIImage?
     
+    
+    @IBOutlet var cameraView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let button = UIButton(type: UIButtonType.Custom)
         
-        let button = UIButton(type: .Custom)
-        button.frame = CGRectMake(0, 0, 64, 64)
         button.clipsToBounds = true
-        
-        button.layer.cornerRadius = 64/2.0
+        button.layer.cornerRadius = 72/2.0
         button.layer.borderColor = UIColor.whiteColor().CGColor
-        button.layer.borderWidth = 2.0
-        button.addTarget(self, action: #selector(ViewController.cameraButton(_:)) , forControlEvents: .TouchUpInside)
+        button.backgroundColor = UIColor.lightGrayColor().colorWithAlphaComponent(0.45)
+        button.layer.borderWidth = 4.0
+        button.addTarget(self, action: #selector(CameraController.cameraButton(_:)) , forControlEvents: .TouchDown)
         
-        view.addSubview(button)
+        self.view.addSubview(button)
+        
+        button.snp_makeConstraints{ (make) -> Void in
+            make.width.equalTo(72)
+            make.height.equalTo(72)
+        
+            make.bottom.equalTo(self.view.snp_bottom).offset(-20)
+            make.centerX.equalTo(self.view.snp_centerX)
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -37,7 +48,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // Dispose of any resources that can be recreated.
     }
     
-    @IBOutlet var cameraView: UIView!
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         previewLayer?.frame = cameraView.bounds
@@ -121,10 +135,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         
     }
-    
-    func cameraButton (sender: AnyObject) {
-        
-    }
 
+    func cameraButton(sender: UIButton!) {
+        captureSession?.startRunning()
+        didPressTakePhoto()
+    }
 }
 
